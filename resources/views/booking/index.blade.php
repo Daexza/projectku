dd624a6fd9df661e8bc3b2a6d30489824ac6f17b
Lutfania <lutfaniaa14@gmail.com>
2024-12-18 21:14:47 +0700

    accommondation booking -fania

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -58,19 +58,7 @@
                         <label>Booking Date</label>
                         <input type="date" name="date" class="form-control" required>
                     </div>
-<<<<<<< HEAD
-=======
-
-                    @if(isset($room))
-                        <input type="hidden" name="room_id" value="{{ $room->room_id }}">
-                        <div class="col-md-3 mb-3">
-                            <label>Room</label>
-                            <input type="text" class="form-control" value="{{ $room->room_number }} - {{ ucfirst($room->room_type) }}" disabled>
-                        </div>
-                    @endif
->>>>>>> 982d1358759533743810d4815f89d6dc40cc233b
                 </div>
-            
                 <button type="submit" class="btn btn-success">Book Now</button>
             </form>
 
